099fc72d89b415d99437e6ebfbe6979d473f4deb
Dominic Maglione <dcmaglione@gmail.com>
2025-03-30 19:01:59 -0400

    got .com back

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://dcmaglione.dev',
+    site: 'https://dcmaglione.com',
     integrations: [mdx(), sitemap()],
 });
